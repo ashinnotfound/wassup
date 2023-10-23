54c3c7ec9790e1cff42c7e4c3339d7fa5b3f5be1
ashinnotfound <ashinnotfound@qq.com>
2023-10-23 22:19:39 +0800

    feat(result): 新增通用结果类

---
+++ wassup-account/src/main/java/com/ashin/wassup/account/result/CommonResult.java
@@ -0,0 +1,52 @@
+package com.ashin.wassup.account.result;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+/**
+ * 前后端通讯标准
+ *
+ * @author ashinnotfound
+ * @date 2023/01/29
+ */
+@Data
+@Accessors(chain = true)
+public class CommonResult<T> {
+    Integer code;
+
+    boolean isSuccess;
+
+    String message;
+
+    Object data;
+
+    public CommonResult(Integer code, boolean isSuccess, String message) {
+        this.code = code;
+        this.isSuccess = isSuccess;
+        this.message = message;
+    }
+
+    public CommonResult(Integer code, boolean isSuccess, String message, T data) {
+        this.code = code;
+        this.isSuccess = isSuccess;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static CommonResult<Void> operateFailWithMessage(String message) {
+        return new CommonResult<>(ResultCode.FAIL_CODE, false, message);
+    }
+
+    public static CommonResult<Void> operateSuccess(String message) {
+        return new CommonResult<>(ResultCode.SUCCESS_CODE, true, message);
+    }
+
+    public static <P> CommonResult<P> operateSuccess(String message, P data) {
+        return new CommonResult<>(
+                ResultCode.SUCCESS_CODE,
+                true,
+                message,
+                data
+        );
+    }
+}
---
+++ wassup-account/src/main/java/com/ashin/wassup/account/result/ResultCode.java
@@ -0,0 +1,12 @@
+package com.ashin.wassup.account.result;
+
+/**
+ * CommonResult的code常量
+ *
+ * @author ashinnotfound
+ * @date 2023/01/29
+ */
+public class ResultCode {
+    public static final Integer SUCCESS_CODE = 200;
+    public static final Integer FAIL_CODE = 400;
+}
