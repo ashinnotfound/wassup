37fa63017bb1465f95d313b8795beee6f15e8405
ashinnotfound <ashinnotfound@qq.com>
2024-03-25 17:17:51 +0800

    improvement: 解耦aop

--- wassup-common/src/main/java/com/ashin/wassup/common/aop/GlobalExceptionHandler.java
+++ wassup-common/src/main/java/com/ashin/wassup/common/aop/GlobalExceptionHandler.java
@@ -1,12 +1,16 @@
-package com.ashin.wassup.account.aop;
+package com.ashin.wassup.common.aop;
 
 import com.ashin.wassup.common.result.CommonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Objects;
 
 
 /**
@@ -28,9 +32,17 @@ public class GlobalExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(BindException.class)
-    public CommonResult<Void> handleBindException(BindException ex) {
-        log.error("BindException", ex);
-        return CommonResult.operateFailWithMessage(ex.getBindingResult().getFieldError().getDefaultMessage());
+    public CommonResult<Void> handleBindException(BindException ex){
+        String errorMessage = "非法的参数: " + Objects.requireNonNull(ex.getFieldError()).getDefaultMessage();
+        log.error(errorMessage);
+        return CommonResult.operateFailWithMessage(errorMessage);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public CommonResult<Void> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex){
+        log.error("非法的参数", ex);
+        return CommonResult.operateFailWithMessage("非法的参数: " + ex.getLocalizedMessage());
     }
 
     @ResponseBody
